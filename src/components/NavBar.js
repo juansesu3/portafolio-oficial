56704f1b22133937c340bf671f2a329cef54621f
juansesu3 <juanseu3@hotmail.com>
2022-01-09 17:05:09 -0500

    committt

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -137,8 +137,9 @@ console.log(window.screen.width);
                          <Nav.Link href="#action2"><LinkReact to="/technologies" style={{ textDecoration: 'none', color:'white'  }}><h1>Stack of Technologies</h1></LinkReact></Nav.Link>
        
                          <Nav.Link href="#action2"><LinkReact to="/testimonials" style={{ textDecoration: 'none', color:'white'  }}><h1>Testimonials</h1></LinkReact></Nav.Link>
-                         <Nav.Link href="#action2"><LinkReact to="/animation" style={{ textDecoration: 'none', color:'white' }}><h1>Animation</h1></LinkReact></Nav.Link>
+                         {/* <Nav.Link href="#action2"><LinkReact to="/animation" style={{ textDecoration: 'none', color:'white' }}><h1>Animation</h1></LinkReact></Nav.Link> */}
                          <Nav.Link href="#action2"><LinkReact to="/contact" style={{ textDecoration: 'none', color:'white' }}><h1>Contact Me</h1></LinkReact></Nav.Link>
+                         <Nav.Link href="#action2"><LinkReact to="/footer" style={{ textDecoration: 'none', color:'white' }}><h1>Footer</h1></LinkReact></Nav.Link>
                          </center>
                
              </Nav>
