c0d15ce16ec95ed7dbfe0caf01ccd7eb99c16c1a
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2022-11-24 15:51:14 -0500

    working on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,14 @@ import { Link as LinkReact } from 'react-router-dom'
 import { app } from '../fb';
 import '../styles/Animation.css'
 import '../styles/NavBar.css'
+import NavScrollExample from '../components/NavbarDos'
 
+
+import Form from 'react-bootstrap/Form';
+
+
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const NavBar = () => {
 
@@ -44,83 +51,60 @@ const NavBar = () => {
   console.log(window.screen.width);
 
   return (
-    <div>
+    <Navbar style={{ backgroundColor: '#000b53'}}  expand="lg">
+      <Container fluid>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-button-dark-example1" className='btn-cv'>
+            Curriculum
+          </Dropdown.Toggle>
 
-      {/* <form onSubmit={submitHandler}>
-           <input type="file"  onChange={archivoHandler}/>
-           <input type="text" name="nombre" placeholder="nombra tu archivo"/>
-           <button>Enviar</button>
+          <Dropdown.Menu variant="dark">
+            <Dropdown.Item href="#/action-1" active>
+              <Navbar.Brand><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20(3).pdf?alt=media&token=8fa06e58-4d9e-427a-9293-0a050da34e05" target="_blank" download="CV Juan Sebastian Suarez Ramirez" ><Button variant="secondary">English</Button></a></Navbar.Brand>
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">
+              <Navbar.Brand><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20(4).pdf?alt=media&token=d779eac8-360c-4628-9419-077d4d7b74c4" target="_blank" download="CV Juan Sebastian Suarez Ramirez" ><Button variant="secondary">Spanish</Button></a></Navbar.Brand></Dropdown.Item>
+
+          </Dropdown.Menu>
+        </Dropdown>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1"><LinkReact to="/" style={{ textDecoration: 'none', color: 'white' }} >Home</LinkReact></Nav.Link>
+            <Nav.Link href="#action2">
+          <LinkReact to="/projects" style={{ textDecoration: 'none', color: 'white' }}>Projects</LinkReact>
+        </Nav.Link>
+        {/* <Nav.Link href="#action2"><LinkReact to="/animation" style={{ textDecoration: 'none', color: 'white' }}><h1>Animation</h1></LinkReact></Nav.Link> */}
+        <Nav.Link href="#action2">
+          <LinkReact to="/technologies" style={{ textDecoration: 'none', color: 'white' }}>Stack of Technologies</LinkReact>
+        </Nav.Link>
+        <Nav.Link href="#action2">
+          <LinkReact to="/testimonials" style={{ textDecoration: 'none', color: 'white' }}>Testimonials</LinkReact>
+        </Nav.Link>
+        <Nav.Link href="#action2">
+          <LinkReact to="/contact" style={{ textDecoration: 'none', color: 'white' }}>Contact Me</LinkReact>
+        </Nav.Link>
+        <Nav.Link href="#action2">
+          <LinkReact to="/footer" style={{ textDecoration: 'none', color: 'white' }}>Footer</LinkReact>
+        </Nav.Link>
            
-         </form> */}
-
-      {(window.screen.width) > 991
-        ? (<Navbar fixed="top" className='style-nav' expand={false}>
-          <Container fluid>
-          <Dropdown>
-              <Dropdown.Toggle id="dropdown-button-dark-example1" className='btn-cv'>
-                Curriculum
-              </Dropdown.Toggle>
-              <Dropdown.Menu variant="light">
-                <Dropdown.Item href="#/action-1" active>
-                  <Navbar.Brand ><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20Eng.pdf?alt=media&token=c3f47660-d109-4ab0-9356-558c2e2f04bd" target="_blank" ><Button variant="secondary">English</Button></a></Navbar.Brand>
-                </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">
-                  <Navbar.Brand><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20Esp.pdf?alt=media&token=2d79df42-53da-4eca-9545-730d23f8f85d" target="_blank" ><Button variant="secondary">Spanish</Button></a></Navbar.Brand></Dropdown.Item>
-
-              </Dropdown.Menu>
-            </Dropdown>
+          
+          </Nav>
+         
+        </Navbar.Collapse>
 
 
-            <LinkReact to="/" className='link' >Home</LinkReact>
-            <LinkReact to="/projects" className='link'>Projects</LinkReact>
-            {/* <LinkReact to="/animation" className='link'>Animation</LinkReact> */}
-            <LinkReact to="/technologies" className='link'>Stack of Technologies</LinkReact>
-            <LinkReact to="/testimonials" className='link'>Testimonials</LinkReact>
-            <LinkReact to="/contact" className='link'>Contact Me</LinkReact>
-            <LinkReact to="/footer" className='link'>Footer</LinkReact>
-           
-            <img className='image-logo-selfbrand' src="https://res.cloudinary.com/dv08oqgvx/image/upload/v1643217212/AmazonasSprint3/bqsbcwcmpvmidtceuozr.png" />
 
-          </Container>
-        </Navbar>)
-        : (<Navbar fixed="top" className='style-nav' expand="lg">
-          <Container fluid>
-            <Dropdown>
-              <Dropdown.Toggle id="dropdown-button-dark-example1" className='btn-cv'>
-                Curriculum
-              </Dropdown.Toggle>
 
-              <Dropdown.Menu variant="dark">
-                <Dropdown.Item href="#/action-1" active>
-                  <Navbar.Brand><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20(3).pdf?alt=media&token=8fa06e58-4d9e-427a-9293-0a050da34e05" target="_blank" download="CV Juan Sebastian Suarez Ramirez" ><Button variant="secondary">English</Button></a></Navbar.Brand>
-                </Dropdown.Item>
-                <Dropdown.Item href="#/action-2">
-                  <Navbar.Brand><a href="https://firebasestorage.googleapis.com/v0/b/portafolio-b0d04.appspot.com/o/CV%20Juan%20Sebastian%20Suarez%20Ramirez%20(4).pdf?alt=media&token=d779eac8-360c-4628-9419-077d4d7b74c4" target="_blank" download="CV Juan Sebastian Suarez Ramirez" ><Button variant="secondary">Spanish</Button></a></Navbar.Brand></Dropdown.Item>
+   
+       
 
-              </Dropdown.Menu>
-            </Dropdown>
-            <Navbar.Toggle aria-controls="navbar-dark-example" />
-            <Navbar.Collapse id="navbar-dark-example">
-              <Nav><center>
-
-                <br />
-                <Nav.Link href="#action1"><LinkReact to="/" style={{ textDecoration: 'none', color: 'white' }} ><h1>Home</h1></LinkReact></Nav.Link>
-                <Nav.Link href="#action2"><LinkReact to="/projects" style={{ textDecoration: 'none', color: 'white' }}><h1>Projects</h1></LinkReact></Nav.Link>
-                {/* <Nav.Link href="#action2"><LinkReact to="/animation" style={{ textDecoration: 'none', color: 'white' }}><h1>Animation</h1></LinkReact></Nav.Link> */}
-                <Nav.Link href="#action2"><LinkReact to="/technologies" style={{ textDecoration: 'none', color: 'white' }}><h1>Stack of Technologies</h1></LinkReact></Nav.Link>
-                <Nav.Link href="#action2"><LinkReact to="/testimonials" style={{ textDecoration: 'none', color: 'white' }}><h1>Testimonials</h1></LinkReact></Nav.Link>
-                <Nav.Link href="#action2"><LinkReact to="/contact" style={{ textDecoration: 'none', color: 'white' }}><h1>Contact Me</h1></LinkReact></Nav.Link>
-                <Nav.Link href="#action2"><LinkReact to="/footer" style={{ textDecoration: 'none', color: 'white' }}><h1>Footer</h1></LinkReact></Nav.Link>
-              </center>
-
-              </Nav>
-            </Navbar.Collapse>
-
-          </Container>
-        </Navbar>
-        )}
-    
-    </div>
+      </Container>
+    </Navbar>
   )
 }
 
