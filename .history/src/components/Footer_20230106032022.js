46f40148da8a856d2f738faf35f781723a766625
Juan Sebastian Suarez Ramirez <90219339+juansesu3@users.noreply.github.com>
2023-01-06 09:22:18 +0100

    fixing problems whit rel=

--- .history/src/components/Footer_20230106032022.js
+++ .history/src/components/Footer_20230106032022.js
@@ -21,9 +21,9 @@ const Footer = () => {
 
                     <Grid4foot> <a style={{color: "#0a66c2"}} href='https://www.linkedin.com/in/juan-s-suarez/' target="_blank" rel="noreferrer"> <FontAwesomeIcon icon={faLinkedin} /></a></Grid4foot>
 
-                    <Grid5foot><a  style={{color: "white"}} href='https://github.com/juansesu3?tab=repositories' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a> </Grid5foot>
+                    <Grid5foot><a  style={{color: "white"}} href='https://github.com/juansesu3?tab=repositories' target="_blank"><FontAwesomeIcon icon={faGithub} /></a> </Grid5foot>
                     
-                    <Grid6foot><a  style={{color: "#38dc57"}}  href='https://api.whatsapp.com/send?phone=41782645121&text=Hi,%20Juan%20Se!%20How%20are%20you?%20We%20are%20interested%20in%20your%20nice%20work,%20lets%20talk%20' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faWhatsappSquare} /></a></Grid6foot>
+                    <Grid6foot><a  style={{color: "#38dc57"}} href='https://api.whatsapp.com/send?phone=573107437640&text=Hola!%20Juan%20se,%20como%20estas?%20Estamos%20interesados%20en%20tu%20trabajo,%20hablmos%20' target="_blank"><FontAwesomeIcon icon={faWhatsappSquare} /></a></Grid6foot>
 
                 </Gridlayoutfooter>
         </div>
